102b6d9e4ab4301b4d2e0f4f78fd9d5c4d8eee1d
Alexey Avdeev <aad.jerry@gmail.com>
2015-03-31 22:14:22 +0300

    форма

--- coffee/application.coffee
+++ coffee/application.coffee
@@ -1,2 +1,43 @@
 $ ->
-  sqrt = (x) -> x * x
+  $restrictions = $('#restrictions')
+
+  $('#input-data').on 'submit', (e) ->
+    e.preventDefault()
+
+  $('#calculate').on 'click', ->
+    console.log 'Вычислить'
+
+  restrictionTemplate = '''
+    <div class="row restriction" style="margin-bottom: 15px;">
+      <div class="col-xs-12">
+        <div class="form-group">
+          <div class="input-group number"></div>
+          <div class="input-group">
+            <input type="text" class="form-control" maxlength="3" placeholder="a[0]" style="width: 50px;">
+            <div class="input-group-addon">x</div>
+          </div>
+          <div class="input-group">
+            <div class="input-group-addon">+</div>
+            <input type="text" class="form-control" maxlength="3" placeholder="a[1]" style="width: 50px;">
+            <div class="input-group-addon">y</div>
+          </div>
+          <div class="input-group">
+            <div class="input-group-addon"><=</div>
+            <input type="text" class="form-control" maxlength="3" placeholder="b" style="width: 50px;">
+            <div class="input-group-addon">b</div>
+          </div>
+        </div>
+        <button type="button" class="btn btn-danger remove">
+          <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+        </button>
+      </div>
+    </div>
+  '''
+
+  $('#restrictions-add').on 'click', ->
+    $restriction = $(restrictionTemplate)
+
+    $restriction.find('.remove').on 'click', ->
+      $restriction.remove()
+
+    $restrictions.append $restriction
--- js/application.js
+++ js/application.js
@@ -1,9 +1,22 @@
 (function() {
   $(function() {
-    var sqrt;
-    return sqrt = function(x) {
-      return x * x;
-    };
+    var $restrictions, restrictionTemplate;
+    $restrictions = $('#restrictions');
+    $('#input-data').on('submit', function(e) {
+      return e.preventDefault();
+    });
+    $('#calculate').on('click', function() {
+      return console.log('Вычислить');
+    });
+    restrictionTemplate = '<div class="row restriction" style="margin-bottom: 15px;">\n  <div class="col-xs-12">\n    <div class="form-group">\n      <div class="input-group number"></div>\n      <div class="input-group">\n        <input type="text" class="form-control" maxlength="3" placeholder="a[0]" style="width: 50px;">\n        <div class="input-group-addon">x</div>\n      </div>\n      <div class="input-group">\n        <div class="input-group-addon">+</div>\n        <input type="text" class="form-control" maxlength="3" placeholder="a[1]" style="width: 50px;">\n        <div class="input-group-addon">y</div>\n      </div>\n      <div class="input-group">\n        <div class="input-group-addon"><=</div>\n        <input type="text" class="form-control" maxlength="3" placeholder="b" style="width: 50px;">\n        <div class="input-group-addon">b</div>\n      </div>\n    </div>\n    <button type="button" class="btn btn-danger remove">\n      <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>\n    </button>\n  </div>\n</div>';
+    return $('#restrictions-add').on('click', function() {
+      var $restriction;
+      $restriction = $(restrictionTemplate);
+      $restriction.find('.remove').on('click', function() {
+        return $restriction.remove();
+      });
+      return $restrictions.append($restriction);
+    });
   });
 
 }).call(this);
