501c3e3f34de02206d26d56b6773ea1e87329851
Alexey Avdeev <aad.jerry@gmail.com>
2015-04-03 09:42:24 +0300

    setDeltaGamma, setMedians

--- coffee/megiddo_solver.coffee
+++ coffee/megiddo_solver.coffee
@@ -1,19 +1,13 @@
 window.App ||= {}
 class App.MegiddoSolver
-  alpha: []
-  I:
-    '-': []
-    '0': []
-    '+': []
-
-  U: []
-
   constructor: (@a, @b, @c) ->
 
   solve: ->
     @setAlpha()
     @setI()
     @setU()
+    @setDeltaGamma()
+    @setMedians()
 
   setAlpha: ->
     @alpha = []
@@ -24,6 +18,11 @@ class App.MegiddoSolver
       @alpha[i][1] = @a[i][1] / @c[1]
 
   setI: ->
+    @I =
+      '-': []
+      '0': []
+      '+': []
+
     for alpha, i in @alpha
       if alpha[1] is 0
         @I['0'].push i
@@ -46,3 +45,55 @@ class App.MegiddoSolver
     @U[1] = Math.min(positives) if positives
 
     # Удалить ограничения из @a, @b
+    @removeRestriction i for i in positives.concat negatives
+
+  removeRestriction: (i) ->
+    @a.splice i, 1 if @a[i]?
+    @b.splice i, 1 if @b[i]?
+
+  isRestrictionExist: (i) -> @a[i]?
+
+  setDeltaGamma: ->
+    @delta = []
+    @gamma = []
+
+    for i in @I['+'].concat @I['-']
+      @delta[i] = - (@alpha[i][0] / @alpha[i][1])
+      @gamma[i] = @b[i] / @alpha[i][1]
+
+  setMedians: ->
+    @medians = []
+
+    for i in @I['+'] by 2
+      if @isRestrictionExist i + 1
+        console.log i, i + 1
+        if @delta[i] is @delta[i + 1]
+          if @gamma[i] > @gamma[i + 1] then @removeRestriction i
+          else @removeRestriction i + 1
+        else
+          x = (@gamma[i + 1] - @gamma[i]) / (@delta[i] - @delta[i + 1])
+          if @U[0]? and x < @U[0]
+            if @delta[i] > @delta[i + 1] then @removeRestriction i
+            else @removeRestriction i + 1
+          else if @U[1]? and x > @U[1]
+            if @delta[i] < @delta[i + 1] then @removeRestriction i
+            else @removeRestriction i + 1
+          else
+            @medians.push x
+
+    for i in @I['-'] by 2
+      if @isRestrictionExist i + 1
+        console.log i, i + 1
+        if @delta[i] is @delta[i + 1]
+          if @gamma[i] < @gamma[i + 1] then @removeRestriction i
+          else @removeRestriction i + 1
+        else
+          x = (@gamma[i + 1] - @gamma[i]) / (@delta[i] - @delta[i + 1])
+          if @U[0]? and x < @U[0]
+            if @delta[i] < @delta[i + 1] then @removeRestriction i
+            else @removeRestriction i + 1
+          else if @U[1]? and x > @U[1]
+            if @delta[i] > @delta[i + 1] then @removeRestriction i
+            else @removeRestriction i + 1
+          else
+            @medians.push x
--- js/megiddo_solver.js
+++ js/megiddo_solver.js
@@ -2,16 +2,6 @@
   window.App || (window.App = {});
 
   App.MegiddoSolver = (function() {
-    MegiddoSolver.prototype.alpha = [];
-
-    MegiddoSolver.prototype.I = {
-      '-': [],
-      '0': [],
-      '+': []
-    };
-
-    MegiddoSolver.prototype.U = [];
-
     function MegiddoSolver(a, b, c) {
       this.a = a;
       this.b = b;
@@ -21,7 +11,9 @@
     MegiddoSolver.prototype.solve = function() {
       this.setAlpha();
       this.setI();
-      return this.setU();
+      this.setU();
+      this.setDeltaGamma();
+      return this.setMedians();
     };
 
     MegiddoSolver.prototype.setAlpha = function() {
@@ -40,6 +32,11 @@
 
     MegiddoSolver.prototype.setI = function() {
       var alpha, i, _i, _len, _ref, _results;
+      this.I = {
+        '-': [],
+        '0': [],
+        '+': []
+      };
       _ref = this.alpha;
       _results = [];
       for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
@@ -58,7 +55,7 @@
     };
 
     MegiddoSolver.prototype.setU = function() {
-      var alpha, i, negatives, positives, _i, _len, _ref;
+      var alpha, i, negatives, positives, _i, _j, _len, _len1, _ref, _ref1, _results;
       this.U = [];
       positives = [];
       negatives = [];
@@ -77,8 +74,113 @@
         this.U[0] = Math.max(negatives);
       }
       if (positives) {
-        return this.U[1] = Math.min(positives);
+        this.U[1] = Math.min(positives);
       }
+      _ref1 = positives.concat(negatives);
+      _results = [];
+      for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+        i = _ref1[_j];
+        _results.push(this.removeRestriction(i));
+      }
+      return _results;
+    };
+
+    MegiddoSolver.prototype.removeRestriction = function(i) {
+      if (this.a[i] != null) {
+        this.a.splice(i, 1);
+      }
+      if (this.b[i] != null) {
+        return this.b.splice(i, 1);
+      }
+    };
+
+    MegiddoSolver.prototype.isRestrictionExist = function(i) {
+      return this.a[i] != null;
+    };
+
+    MegiddoSolver.prototype.setDeltaGamma = function() {
+      var i, _i, _len, _ref, _results;
+      this.delta = [];
+      this.gamma = [];
+      _ref = this.I['+'].concat(this.I['-']);
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        i = _ref[_i];
+        this.delta[i] = -(this.alpha[i][0] / this.alpha[i][1]);
+        _results.push(this.gamma[i] = this.b[i] / this.alpha[i][1]);
+      }
+      return _results;
+    };
+
+    MegiddoSolver.prototype.setMedians = function() {
+      var i, x, _i, _j, _len, _len1, _ref, _ref1, _results;
+      this.medians = [];
+      _ref = this.I['+'];
+      for (_i = 0, _len = _ref.length; _i < _len; _i += 2) {
+        i = _ref[_i];
+        if (this.isRestrictionExist(i + 1)) {
+          console.log(i, i + 1);
+          if (this.delta[i] === this.delta[i + 1]) {
+            if (this.gamma[i] > this.gamma[i + 1]) {
+              this.removeRestriction(i);
+            } else {
+              this.removeRestriction(i + 1);
+            }
+          } else {
+            x = (this.gamma[i + 1] - this.gamma[i]) / (this.delta[i] - this.delta[i + 1]);
+            if ((this.U[0] != null) && x < this.U[0]) {
+              if (this.delta[i] > this.delta[i + 1]) {
+                this.removeRestriction(i);
+              } else {
+                this.removeRestriction(i + 1);
+              }
+            } else if ((this.U[1] != null) && x > this.U[1]) {
+              if (this.delta[i] < this.delta[i + 1]) {
+                this.removeRestriction(i);
+              } else {
+                this.removeRestriction(i + 1);
+              }
+            } else {
+              this.medians.push(x);
+            }
+          }
+        }
+      }
+      _ref1 = this.I['-'];
+      _results = [];
+      for (_j = 0, _len1 = _ref1.length; _j < _len1; _j += 2) {
+        i = _ref1[_j];
+        if (this.isRestrictionExist(i + 1)) {
+          console.log(i, i + 1);
+          if (this.delta[i] === this.delta[i + 1]) {
+            if (this.gamma[i] < this.gamma[i + 1]) {
+              _results.push(this.removeRestriction(i));
+            } else {
+              _results.push(this.removeRestriction(i + 1));
+            }
+          } else {
+            x = (this.gamma[i + 1] - this.gamma[i]) / (this.delta[i] - this.delta[i + 1]);
+            if ((this.U[0] != null) && x < this.U[0]) {
+              if (this.delta[i] < this.delta[i + 1]) {
+                _results.push(this.removeRestriction(i));
+              } else {
+                _results.push(this.removeRestriction(i + 1));
+              }
+            } else if ((this.U[1] != null) && x > this.U[1]) {
+              if (this.delta[i] > this.delta[i + 1]) {
+                _results.push(this.removeRestriction(i));
+              } else {
+                _results.push(this.removeRestriction(i + 1));
+              }
+            } else {
+              _results.push(this.medians.push(x));
+            }
+          }
+        } else {
+          _results.push(void 0);
+        }
+      }
+      return _results;
     };
 
     return MegiddoSolver;
